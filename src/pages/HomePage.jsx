06a529b5a63caf24dcef5160e7a99d6ba64da948
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2025-01-23 19:27:40 +0700

    develop home page design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,45 @@
+import HeroImage from "../assets/images/hero.svg";
+
 const HomePage = () => {
   return (
-    <div className="h-[2000px]">HomePage</div>
-  )
-}
+    <div className="homepage pb-10">
+      <div className="container mx-auto px-4">
+        <div className="hero grid md:grid-cols-2 grid-cols-1 items-center gap-20 pt-32">
+          <div className="box">
+            <h1 className="lg:text-5xl/tight text-3xl font-medium mb-7">
+              Belajar Membuat Website Dengan{" "}
+              <span className="font-bold text-sky-400 underline">
+                {" "}
+                Tailwind CSS
+              </span>
+            </h1>
+            <p className="text-base/8 mb-7">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <a
+              href="#"
+              className="bg-sky-400 hover:bg-sky-500 transition-all py-2 px-4 text-white shadow rounded-full"
+            >
+              Tentang Kami <i className="ri-eye-line ms-1"> </i>
+            </a>
+          </div>
+          <div className="box">
+            <img
+              src={HeroImage}
+              alt="Hero Image"
+              className="md:w-full w-[400px] mx-auto md:m-0"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
